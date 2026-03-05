99ff6d1046d4de69bd1bb0c321b36f8e56d4be33
Richard <richard1simonik@gmail.com>
2026-03-05 10:08:22 +0100

    Revamped version as i fucked the first one

--- Leetcode/2AddTwoNumber.swift
+++ Leetcode/2AddTwoNumber.swift
@@ -21,19 +21,56 @@ public class ListNode {
 
 class Solution {
     static func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var num1 = getNumber(l1)
-        var num2 = getNumber(l2)
-    
-        var sum = String(String(num1+num2).reversed())
-        var lastNode = ListNode(Int(String(sum[sum.count-1]))!)
-        for i in (0..<sum.count-1).reversed(){
-            let node = ListNode(Int(String(sum[i]))!, lastNode)
-            lastNode = node
+        guard let start1 = l1 else{
+            if l2 != nil{
+                return l2
+            }
+            return nil
         }
+        guard let start2 = l2 else{
+            return start1
+        }
+        var carry = 0
+        var head1:ListNode? = start1
+        var head2:ListNode? = start2
+        var lastNode:ListNode? = nil
+        let sum1 = head1!.val+head2!.val+carry
+        if ((sum1/10) != 0){
+                carry = 1
+        }else{
+                carry = 0
+        }
+        let node1 = ListNode(sum1%10, lastNode)
+        lastNode = node1
+        repeat{
+            head1 = head1!.next
+            head2 = head2!.next
+            if (head1==nil && head2==nil){
+                break
+            }
+            if (head1==nil){
+                lastNode = head2
+                break
+            }
+            if head2==nil{
+                lastNode = head1
+                break
+            }
+            let sum = head1!.val+head2!.val+carry
+            if ((sum/10) != 0){
+                carry = 1
+            }else{
+                carry = 0
+            }
+            let node = ListNode(sum%10, lastNode)
+            lastNode = node
+        }while(head1!.next != nil || head2!.next != nil)
+        
         return lastNode
     }
+}
 
-    static func getNumber(_ l:ListNode?)->Int{
+func getNumber(_ l:ListNode?)->Int{
         if let node = l{
             var currentNode:ListNode? = node
             var currentScale = 10
@@ -50,35 +87,9 @@ class Solution {
             return finalNumber
         }
         return 0
-    }
 }
 
-extension String {
 
-    var length: Int {
-        return count
-    }
-
-    subscript (i: Int) -> String {
-        return self[i ..< i + 1]
-    }
-
-    func substring(fromIndex: Int) -> String {
-        return self[min(fromIndex, length) ..< length]
-    }
-
-    func substring(toIndex: Int) -> String {
-        return self[0 ..< max(0, toIndex)]
-    }
-
-    subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
-                                            upper: min(length, max(0, r.upperBound))))
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return String(self[start ..< end])
-    }
-}
 
 
 
@@ -87,7 +98,6 @@ var middleNode = ListNode(4,endNode)
 var startNode = ListNode(3,middleNode)
 
 var endNode2 = ListNode(5)
-var middleNode2 = ListNode(6,endNode2)
-var startNode2 = ListNode(4,middleNode2)
+var startNode2 = ListNode(4,endNode2)
 
-print(Solution.getNumber(Solution.addTwoNumbers(startNode, startNode2)))
+print(getNumber(Solution.addTwoNumbers(startNode, startNode2)))
