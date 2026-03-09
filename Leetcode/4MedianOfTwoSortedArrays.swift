8847c08d9c62cd686c563789eaf6c82bc8c855aa
Richard <richard1simonik@gmail.com>
2026-03-09 09:08:19 +0100

    improved version of the findmedian

--- Leetcode/4MedianOfTwoSortedArrays.swift
+++ Leetcode/4MedianOfTwoSortedArrays.swift
@@ -6,6 +6,66 @@ import Foundation
 
 class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+        var head1 = 0
+        var head2 = 0
+        let totalLen = (nums1.count+nums2.count)
+        if totalLen%2==0{
+            var lastElem = 0
+            var lastLastElem = 0
+            print((totalLen / 2))
+            while(((head1+head2)<((totalLen / 2)+1))){
+                lastLastElem = lastElem
+                if head1 == nums1.count{
+                    lastElem=nums2[head2]
+                     head2+=1
+                    continue
+                }else if head2 == nums2.count{
+                    lastElem=nums1[head1]
+                    head1+=1
+                    continue
+                }
+
+                if nums1[head1]<nums2[head2]{
+                    lastElem=nums1[head1]
+                    head1+=1
+                }else{
+                    lastElem=nums2[head2]
+                    head2+=1
+                }
+            }
+            print("last elem \(lastElem) adn alast last \(lastLastElem)")
+            print((lastElem+lastLastElem)/2)
+            return Double((Double(lastElem)+Double(lastLastElem))/2)
+        }else{
+            var lastElem = 0
+            print((totalLen / 2))
+            while((head1+head2) <= ((totalLen / 2))){
+                if head1 == nums1.count{
+                    lastElem=nums2[head2]
+                    head2+=1
+                    continue
+                }else if head2 == nums2.count{
+                    lastElem=nums1[head1]
+                    head1+=1
+                    continue
+                }
+
+                if nums1[head1]<nums2[head2]{
+                    lastElem=nums1[head1]
+                    head1+=1
+                }else{
+                    lastElem=nums2[head2]
+                    head2+=1
+                }
+                print("last elem \(lastElem)")
+            }
+            return Double(lastElem)
+        }
+        
+    }
+
+
+    func findMedianSortedArraysOld(_ nums1: [Int], _ nums2: [Int]) -> Double {
         // let middle = (nums1.count+nums2.count).round(.up)
         // if (middle%2)==0
         // let m = nums1.count
@@ -51,3 +111,4 @@ class Solution {
 
 print(Solution().findMedianSortedArrays([2,2,4,4],[2,2,2,4,4]))
 //print(Solution().findMedianSortedArrays([1,2],[3,4]))
+//print(Solution().findMedianSortedArrays([1,3],[2]))
