6c36a250ec3741e8bf9d1e009e3158af582ea15d
Richard <richard1simonik@gmail.com>
2026-03-09 16:51:09 +0100

    1200 in more efficent way

--- Leetcode/1200MinimalAbsoluteDiff.swift
+++ Leetcode/1200MinimalAbsoluteDiff.swift
@@ -1,23 +1,42 @@
 class Solution {
-    func minimumAbsDifference(_ arr: [Int]) -> [[Int]] {
+     func minimumAbsDifference(_ arr: [Int]) -> [[Int]] {
         var lowest:Int? = nil 
-        var occurLow:Int = 0
+        var occurLow:[[Int]] = []
+        let newArr = arr.sorted()
         
-        for i in 1..<arr.count{
+        for i in 0..<arr.count-1{
+            var diff = newArr[i+1]-newArr[i]
+            if lowest==nil || diff<lowest!{
+                lowest = diff
+                occurLow = [[newArr[i],newArr[i+1]]]
+            }else if diff==lowest{
+                occurLow.append([newArr[i],newArr[i+1]])
+            }
+        }
+        return occurLow
+    }
+
+
+
+
+    func minimumAbsDifferenceNaive(_ arr: [Int]) -> [[Int]] {
+        var lowest:Int? = nil 
+        var occurLow:[[Int]] = []
+        
+        for i in 0..<arr.count{
             for j in i..<arr.count{
-                if lowest==nil{
-                    lowest = [i]-[i-1]
-                    occurLow = 1
+                if i==j{
+                    continue
                 }
-                var diff = [i]-[i-1]
-                if diff<lowest!{
-                    lowest = [i]-[i-1]
-                    occurLow = 1
+                var diff = max(arr[i],arr[j])-min(arr[j],arr[i])
+                if lowest==nil || diff<lowest!{
+                    lowest = diff
+                    occurLow = [[min(arr[i],arr[j]),max(arr[i],arr[j])]]
                 }else if diff==lowest{
-                     occurLow += 1
+                     occurLow.append([min(arr[i],arr[j]),max(arr[i],arr[j])])
                 }
             }
         }
-
+        return occurLow.sorted{$0[0] < $1[0]}
     }
 }
