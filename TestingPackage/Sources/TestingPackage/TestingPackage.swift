98e4508efe1bda4d25a2c8971ab92d8f547850da
Richard <richard1simonik@gmail.com>
2026-03-11 10:00:10 +0100

    created the swift package to debug the code and slow but working version of the 5 palindrome works

--- TestingPackage/Sources/TestingPackage/TestingPackage.swift
+++ TestingPackage/Sources/TestingPackage/TestingPackage.swift
@@ -5,106 +5,87 @@
 
 class Solution {
     var longestPal = ""
-
+    
+    //Brute force
     func longestPalindrome(_ s: String) -> String {
         let strAsArray = s.map({String($0)})
         longestPal = strAsArray[0]
 
-        if strAsArray.count<3{
-            var head = 0
-            var tail = strAsArray.count-1
-            while head<=tail{
-                if strAsArray[head] != strAsArray[tail]{return longestPal}
-                head+=1
-                tail-=1
-            }
-            return s
-        }
-
-        for i in 0..<s.count{
+        // for every letter have two pointers to the start and end of palindrome and then add either until you no longer can due to size or if addition of both/left/right with according pointer, does not yield palindrome
+        for i in 0..<strAsArray.count{
             var currentPalindrome = "\(strAsArray[i])"
-            var leftBlocked = false
-            var rightBlocked = false
-            for j in 1..<s.count{
-                // If one of the indexes is not valid -> does not work on even palindromes
-                if 0>(i-j) || (i+j)>=s.count{
-                    var added = false
-                    if let char = checkSameChars(currentPalindrome){
-                        if (i-j)>0 && !leftBlocked{
-                            if strAsArray[i-j]==char{
-                                print("added edgy left \(char) to \(currentPalindrome)")
-                                currentPalindrome = "\(strAsArray[i-j])"+currentPalindrome
-                                updatePalindrome(currentPalindrome)
-                                added = true
-                            }else{
-                                leftBlocked = true
-                            }
-                        }
-                        if (i+j)<s.count && !rightBlocked{
-                            if strAsArray[i+j]==char{
-                                print("added edgy right \(char) to \(currentPalindrome)")
-                                currentPalindrome = currentPalindrome+"\(strAsArray[i+j])"
-                                updatePalindrome(currentPalindrome)
-                                added = true
-                            }else{
-                                rightBlocked = true
-                            }
-                        }
-                    }
-                    // TODO: Issue is here cause it can happen taht you have aaaa, so there is no middle point, the option to still append when on is still valid, technically needed only once
-                    if ((i-j)<0 && (i+j)>=s.count) || !added{
-                        break
-                    }
-                    print(currentPalindrome)
+            var leftPointer = i-1
+            var rightPointer = i+1
+            var stopLoop = false
+
+            while !stopLoop{
+                if longestPal.count==s.count{
+                    break
+                }
+
+                let leftCharacter = checkIndexes(strAsArray, leftPointer) ?? ""
+                let rightCharacter = checkIndexes(strAsArray, rightPointer) ?? ""
+                if leftCharacter=="" && ""==rightCharacter{
+                    stopLoop = true
+                    break 
+                }
+
+                if leftCharacter==rightCharacter{
+                    currentPalindrome = "\(strAsArray[leftPointer])"+currentPalindrome+"\(strAsArray[rightPointer])"
+                    updatePalindrome(currentPalindrome)
+                    rightPointer+=1
+                    leftPointer-=1
                     continue
                 }
 
-                // idea to create teh indexes like let charPre:STRING if out of the indexes you just put ""
-
-                //IF asymetrical addition
-                if strAsArray[i-j] != strAsArray[i+j]{
-                    if let char = checkSameChars(currentPalindrome){
-                        print("checked palindrome \(currentPalindrome) and \(strAsArray[i-j]) \(strAsArray[i+j])")
-                        if strAsArray[i+j] != char && strAsArray[i-j] != char{
-                            break
-                        }
-                        if strAsArray[i-j]==char && !leftBlocked{
-                            currentPalindrome = "\(strAsArray[i-j])"+currentPalindrome
-                            updatePalindrome(currentPalindrome)
-                            print("append \(char) to \(currentPalindrome)")
-                        }else{
-                            leftBlocked = true
-                        }
-                        
-                        if strAsArray[i+j]==char && !rightBlocked{
-                            currentPalindrome = currentPalindrome+"\(strAsArray[i+j])"
-                            updatePalindrome(currentPalindrome)
-                            print("prepend \(char) to \(currentPalindrome)")
-                        }else{
-                           rightBlocked = true
-                        }
-                    }else{
-                        break
-                    }
-                // IF the pre and after are the same just append both
-                }else{
-                    currentPalindrome = "\(strAsArray[i-j])"+currentPalindrome+"\(strAsArray[i+j])"
-                    print(currentPalindrome)
+                var change = false
+                if leftCharacter != "" && checkPalindrome(leftCharacter + currentPalindrome){
+                    currentPalindrome = leftCharacter+currentPalindrome
                     updatePalindrome(currentPalindrome)
+                    leftPointer-=1
+                    change = true
                 }
 
-                //Check if the palindrome is not the whole word
-                if longestPal.count==strAsArray.count{
-                    break
+                if rightCharacter != "" && checkPalindrome(currentPalindrome+rightCharacter){
+                    currentPalindrome = currentPalindrome+rightCharacter
+                    updatePalindrome(currentPalindrome)
+                    rightPointer+=1
+                    change = true
+                }
+
+                if !change{
+                    stopLoop=true
                 }
             }
         }
         return longestPal
     }
 
+   func checkPalindrome(_ potential: String) -> Bool {
+    let arr = Array(potential)
+    var head = 0
+    var tail = arr.count - 1
+        while head < tail {
+            if arr[head] != arr[tail] {
+                return false
+            }
+            head += 1
+            tail -= 1
+        }
+    return true
+    }
+
+    func checkIndexes(_ arr:[String], _ index:Int) -> String?{
+        if index<0 || index>=arr.count{
+            return nil
+        }else{
+            return arr[index]
+        }
+    }
+
     func updatePalindrome(_ curr:String){
         if curr.count>longestPal.count{
-            print("updating \(curr) instead of \(longestPal)")
+            //print("updating \(curr) instead of \(longestPal)")
             longestPal=curr
         }
     }
@@ -121,12 +102,12 @@ class Solution {
     }
 }
 
-//print(Solution().longestPalindrome("babad"))
-//print(Solution().longestPalindrome("aacabdkacaa"))
+print(Solution().longestPalindrome("babad"))
+print(Solution().longestPalindrome("aacabdkacaa"))
 
 print(Solution().longestPalindrome("tattarrattat"))
 
-// print(Solution().longestPalindrome("a"))
-// print(Solution().longestPalindrome("aba"))
-// print(Solution().longestPalindrome("abb"))
+ print(Solution().longestPalindrome("a"))
+ print(Solution().longestPalindrome("aba"))
+ print(Solution().longestPalindrome("abb"))
 
