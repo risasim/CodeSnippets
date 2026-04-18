822c757087288fa904699aae36638b2025b48417
Richard <richard1simonik@gmail.com>
2026-04-18 09:49:39 +0200

    cannto seem to make the cipher work

--- Leetcode/2075DecipherSlentedText.swift
+++ Leetcode/2075DecipherSlentedText.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 class Solution {
-    func decodeCiphertext(_ encodedText: String, _ rows: Int) -> String {
+    func originalDecodeCiphertext(_ encodedText: String, _ rows: Int) -> String {
         let numOfChars = rows-1
         var stringArray = encodedText.components(separatedBy: "   ")
         print(stringArray)
@@ -22,8 +22,51 @@ class Solution {
         } 
         return finalText
     }
+
+    func decodeCiphertext(_ encodedText: String, _ rows: Int) -> String {
+        if encodedText == ""{
+            return ""
+        }
+        let numOfTheOtherRows = (encodedText.count/rows)
+        var stringArray = encodedText.components(withMaxLength: numOfTheOtherRows)
+        print(stringArray)
+        var longest = 0
+        for str in stringArray{
+            let trimCount = str.trimmingCharacters(in: .whitespaces).count
+            if trimCount>longest{
+                longest = trimCount
+            }
+        }
+        var added = true
+        var finalText = ""
+        for j in 0..<longest{
+            for i in 0..<rows{
+                if i+j>=stringArray[0].count{
+                    break
+                }
+                finalText += stringArray[i][i+j]
+            }
+        }
+        return finalText.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression)
+    }
 }
 
-print(Solution().decodeCiphertext(" b  ac", 2))
-print(Solution().decodeCiphertext("iveo    eed   l te   olc", 2))
-print(Solution().decodeCiphertext("ch   ie   pr", 2))
+extension String {
+    func components(withMaxLength length: Int) -> [String] {
+        return stride(from: 0, to: self.count, by: length).map {
+            let start = self.index(self.startIndex, offsetBy: $0)
+            let end = self.index(start, offsetBy: length, limitedBy: self.endIndex) ?? self.endIndex
+            return String(self[start..<end])
+        }
+    }
+}
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+print(Solution().decodeCiphertext("             a     b", 4))
+//print(Solution().decodeCiphertext("iveo    eed   l t    olc", 4))
+//print(Solution().decodeCiphertext("ch   ie   pr", 3))
